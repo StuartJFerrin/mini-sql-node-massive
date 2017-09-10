b8d82855849fb5d6d7878c54a03dd21795f585ca
Stuart J Ferrin <stuart.j.ferrin@gmail.com>
2017-09-09 20:46:50 -0600

    September Update

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const connectionString = "postgres://postgres:Romania7@localhost/sandbox";
+const controller = require('./controller');
 
 const app = module.exports = express();
+massive(connectionString).then(dbInstance => {
+    app.set('db', dbInstance);
+});
+
 app.use( bodyParser.json() );
 app.use( cors() );
+
+app.get('/api/planes', controller.getPlanes);
 
 const port = 3000;
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
